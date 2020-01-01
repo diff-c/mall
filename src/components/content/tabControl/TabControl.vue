ff945447cdc7bb851ff0a32bb423e27f1a2ec5f4
diff-c <2868231146@qq.com>
2020-01-01 17:39:03 +0800

    2020/1/1

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" :key="item" class="tab-control-item" :class="{active:index===currentIndex}"
+      @click="changeIndex(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    changeIndex(index) {
+      this.currentIndex = index;
+    }
+  },
+}
+</script>
+
+<style>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  background-color: pink;
+}
+.tab-control-item {
+  flex: 1;
+  height: 40px;
+  line-height: 40px;
+}
+.active span {
+  padding: 5px;
+  /* display: inline-block; */
+  color: var(--color-high-text);
+  border-bottom: 3px solid var(--color-tint);
+}
+</style>
---
+++ src/views/home/childrenComponents/Recommend.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" :key="item.sort" class="recommends-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <p>{{item.title}}</p>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.recommend {
+  display: flex;
+  padding: 10px;
+  text-align: center;
+  font-size: 12px;
+  padding: 10px 0 20px;
+  border-bottom: 8px solid #eee;
+}
+.recommends-item {
+  flex: 1;
+}
+.recommends-item img {
+  width: 70px;
+  height: 70px;
+  margin-bottom: 10px;
+}
+</style>
